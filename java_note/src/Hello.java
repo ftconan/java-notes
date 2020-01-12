5cdceaa8bf00534254a8dc673958b487198c81ce
ftconan <1508907793@qq.com>
2020-01-12 21:48:31 +0800

    add Hello.class

---
+++ java_note/src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author magician
+ * @version 1.0
+ * @date 2020/1/12
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
