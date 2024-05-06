6d3d2997083121f7c5492bdedff712b26f89a57a
ftconan <1508907793@qq.com>
2024-05-06 14:35:03 +0800

    JUnit

---
+++ JavaSECode/src/com/atguigu/oop/JUnitTest.java
@@ -0,0 +1,21 @@
+package com.atguigu.oop;
+
+import jdk.jfr.StackTrace;
+import org.junit.Test;
+
+/**
+ * @Author magician
+ * @Date 2024/5/6
+ * @Version 1.0
+ */
+public class JUnitTest {
+    @Test
+    public void test1() {
+        System.out.println("test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("test2");
+    }
+}
